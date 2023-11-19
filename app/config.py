2156ec2586dd2d2e75b3380cd796ecef8c379041
idekworks <99700493+idekworks@users.noreply.github.com>
2023-11-19 16:45:03 +0900

    fix: refactaring code, separate file.

---
+++ app/config.py
@@ -0,0 +1,6 @@
+INPUT_FILE_PATH = './input_test_data'
+OUTPUT_PATH = './output_data/'
+
+TASK_CSV_FILE_NAME = 'task.csv'
+SCHEDULE_CSV_FILE_NAME = 'schedule.csv'
+OUTPUT_CSV_FILE_NAME = 'output.csv'
