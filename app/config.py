eb64d1ecc23542534e5431ebe929371b0a62c112
idekworks <99700493+idekworks@users.noreply.github.com>
2023-11-19 16:52:26 +0900

    feat: add calculate schedule function

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 INPUT_FILE_PATH = './input_test_data'
-OUTPUT_PATH = './output_data/'
+OUTPUT_FILE_PATH = './output_data/'
 
 TASK_CSV_FILE_NAME = 'task.csv'
 SCHEDULE_CSV_FILE_NAME = 'schedule.csv'
--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 import csv
 import os
 
-from config import INPUT_FILE_PATH, TASK_CSV_FILE_NAME, SCHEDULE_CSV_FILE_NAME
+from config import INPUT_FILE_PATH, TASK_CSV_FILE_NAME, SCHEDULE_CSV_FILE_NAME, OUTPUT_FILE_PATH, OUTPUT_CSV_FILE_NAME
 from csv_reader import read_task_csv, read_schedule_csv 
 
 # ファイルパスを適切なものに置き換えてください
-header, schedule_data = read_schedule_csv(os.path.join(INPUT_FILE_PATH, SCHEDULE_CSV_FILE_NAME))
+schedule_header, schedule_data = read_schedule_csv(os.path.join(INPUT_FILE_PATH, SCHEDULE_CSV_FILE_NAME))
 task_data = read_task_csv(os.path.join(INPUT_FILE_PATH, TASK_CSV_FILE_NAME))
+
+output_path = os.path.join(OUTPUT_FILE_PATH, OUTPUT_CSV_FILE_NAME)
+def calculate_schedule(schedule_header, schedule_data, task_data, output_path):
+    return []
 
 # 読み込んだデータの表示
 for row in schedule_data:
@@ -15,5 +19,5 @@ for row in schedule_data:
 # データの表示（確認用）
 for task in task_data:
     print(task)
-print(header)
+print(schedule_header)
 
